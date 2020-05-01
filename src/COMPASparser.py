bce9df8bc698e584bd7c87b58ba7b5f0f50227cf
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-05-01 17:31:07 +1000

    black file and add docs

--- src/COMPASparser.py
+++ src/COMPASparser.py
@@ -1,179 +1,187 @@
-import sys
-import numpy as np
+""" Module to parse a COMPAS file
+"""
+
 import argparse
 
+import numpy as np
 
-# process file - read, parse and return data
-#
-# reads the file passed as parameter 'filename'
-# parses record using delimiter passed as parameter 'delimiter'
-#
-# returns lists:
-#
-#    names  - 1-d list of column names (headings)
-#    types  - 1-d list of column data types
-#    units  - 1-d list of column units
-#    values - 2-d list of data values: values[column][row]
-#
-#    all returned lists will be empty on failure (e.g. unable to open file)
 
-def ProcessFile(filename, delimiter):
+def parse_header(header_str, delimiter):
+    """
 
-    names  = []
-    types  = []
-    units  = []
-    values = []
+    :param header_str:
+    :param delimiter:
+    :return:
+    """
+    headers = header_str.split(delimiter)  # split header using delimiter
+    headers = [h.strip() for h in headers]  # remove trailing whitespaces in each header
+    return headers
 
-    # read and parse the data from the file
 
+def parse_data(filehandler, delimiter):
+    """
+    Read and parse the data from the file
+    data values are extracted to 2-d list ([column][row])
+
+    :param filehandler:
+    :param delimiter:
+    :return:
+    """
+    data = np.genfromtxt(
+        fname=filehandler,
+        skip_header=2,
+        delimiter=delimiter,
+        autostrip=True,
+        names=True
+    )
+    names = data.dtype.names
+
+    # initalise an empty list of values
+    num_rows, num_cols = len(data), len(names)
+    values = [[0.0 for _ in range(num_rows)] for _ in range(num_cols)]
+    # read the data from the file into 2-d values list
+    for row in range(num_rows):
+        # extract the data values to a 2-d list
+        for column in range(min(num_cols, len(data[row]))):
+            values[column][row] = data[row][column]
+
+    return values, names
+
+
+def process_file(filename, delimiter=','):
+    """Read and return the names, types, units and values stored in a COMPAS file.
+
+    If there is a failure, all returned lists will be empty (e.g. unable to open file).
+
+    :param filename: str
+        The COMPAS data filepath.
+    :param delimiter: str
+        Character for specifying the boundary between separate cols in the COMPAS file.
+        Default is ','
+    :return: Tuple(list, list, list, list)
+        names  - 1-d list of column names (headings)
+        types  - 1-d list of column data types
+        units  - 1-d list of column units
+        values - 2-d list of data values: values[column][row]
+
+    """
+    with open(filename, mode='r') as fi:
+        types = parse_header(fi.readline(), delimiter)  # types on 1st line
+        units = parse_header(fi.readline(), delimiter)  # units on 2nd line
+        fi.seek(0)  # reset filehandler
+        values, names = parse_data(fi, delimiter)
+        return names, types, units, values
+
+
+def get_filename_from_cli():
+    """
+    :return: str
+        Filename entered in CLI
+    """
+    parser = argparse.ArgumentParser(description='COMPAS data file parse example.')
+    parser.add_argument(
+        'filename',
+        metavar='filename',
+        type=str,
+        nargs=1,
+        help='name of COMPAS data file'
+    )
+    args = parser.parse_args()
+    return args.filename
+
+
+def generate_data_summary(names, types, units, values):
+    """
+
+    :param names:
+    :param types:
+    :param units:
+    :param values:
+    :return:
+    """
+    output_data = []
+
+    # Counts
+    output_data.append(f'column names  count = {len(names)}')
+    output_data.append(f'column types  count = {len(types)}')
+    output_data.append(f'values column count = {len(values)}')
+    output_data.append(f'values row    count = {len(values[0])}')
+
+    # Column names and types and units
+    output_data.append('\nColumn names:\n')
+    for idx, n in enumerate(names):
+        output_data.append(f'Column{idx}, Name = {n}')
+    output_data.append('\nColumn types:\n')
+    for idx, t in enumerate(types):
+        output_data.append(f'Column{idx}, Type = {t}')
+    output_data.append('\nColumn units:\n')
+    for idx, u in enumerate(units):
+        output_data.append(f'Column{idx}, Units = {u}')
+
+    # the data can be accessed by value[column][row]
+    # print a couple of values
+
+    if len(values) > 4 and len(values[0]) > 14:
+        print()
+
+        column = 3
+        row = 5
+        print(
+            'Column', column, ', name =', names[column], ', type =', types[column],
+            ', units =',
+            units[column], ', value at row', row, '=', values[column][row])
+
+        column = 4
+        row = 14
+        print(
+            'Column', column, ', name =', names[column], ', type =', types[column],
+            ', units =',
+            units[column], ', value at row', row, '=', values[column][row])
+
+    # look for a specific column name
+
+    columnName = 'Radius_1'
     try:
-        fi = open(filename)
+        whichColumn = names.index(columnName)
+    except ValueError:
+        print('\nColumn name', columnName, 'not found!')
+        whichColumn = -1
 
-        types = fi.readline().split(delimiter)                      # read types header record and split on delimiter
-        types = [t.strip() for t in types]                          # strip leading and trailing whitespace (including \n) from each element in list
+    if whichColumn >= 0:
+        print('\nColumn name', columnName, 'is column index', whichColumn, '(0-based)')
 
-        units = fi.readline().split(delimiter)                      # read units header record and split on delimiter
-        units = [u.strip() for u in units]                          # strip leading and trailing whitespace (including \n) from each element in list
+    # print a couple of values from the column just found
 
-        # rewind file to beginning, read and parse the data from the file
-        # data values are extracted to 2-d list ([column][row])
+    if len(values[whichColumn]) > 17:
+        print()
 
-        fi.seek(0)
-        data = np.genfromtxt(fi, skip_header = 2, delimiter = delimiter, deletechars = " !#$%&'*+,.:;=?[\]{|}~", names = True)
+        column = whichColumn
 
-        try:
-            fi.close()
+        row = 3
+        print(
+            'Column', column, ', name =', names[column], ', type =', types[column],
+            ', units =',
+            units[column], ', value at row', row, '=', values[column][row])
 
-            names = data.dtype.names                                                                # record column names
-
-            values = [[0.0 for y in range(len(data))] for x in range(len(data.dtype.names))]        # size the 2-d values list
-
-            # read the data from the file into 2-d values list
-            for row in range(len(data)):
-
-                # extract the data values to a 2-d list
-                for column in range(min(len(data.dtype.names), len(data[row]))):
-                    values[column][row] = data[row][column]
-
-        except (OSError, IOError):
-            print('Close of file ', filename ,' failed')
-
-    except (OSError, IOError):
-        print('Unable to open file ', filename)
-
-    return (names, types, units, values)
+        row = 16
+        print(
+            'Column', column, ', name =', names[column], ', type =', types[column],
+            ', units =',
+            units[column], ', value at row', row, '=', values[column][row])
 
 
-#
-# Main program
 
-# check we have one file to process
-
-parser = argparse.ArgumentParser(description = 'COMPAS data file parse example.')
-
-parser.add_argument('filename', metavar = 'filename', type = str, nargs = 1, help = 'name of COMPAS data file')
-
-args = parser.parse_args()
+    data_summary = "\n".join(output_data)
+    return data_summary
 
 
-# process file
-
-print('\nProcessing file', args.filename[0], '\n')
-
-names, types, units, values = ProcessFile(args.filename[0], ',')            # use this line for COMMA delimited files
-
-#names, types, units, values = ProcessFile(args.filename[0], '\t')           # use this line for TAB delimited files
-
-
-# check for failure
-
-if len(values) < 2:
-    print('Parse failed')
-    sys.exit(1)
+def main():
+    filename = get_filename_from_cli()
+    print(f'Processing file {filename}')
+    names, types, units, values = process_file(filename)
+    data_summary = generate_data_summary(names, types, units, values)
+    print(data_summary)
 
 
-# print counts
-
-print('column names  count =', len(names))
-print('column types  count =', len(types))
-print('values column count =', len(values))
-print('values row    count =', len(values[0]))
-
-
-# print column names
-
-print('\nColumn names:\n')
-column = 0
-for n in names:
-    print('Column', column, ', Name =', n)
-    column += 1
-
-
-# print column types
-
-print('\nColumn types:\n')
-column = 0
-for t in types:
-    print('Column', column, ', Type =', t)
-    column += 1
-
-
-# print column units
-
-print('\nColumn units:\n')
-column = 0
-for u in units:
-    print('Column', column, ', Units =', u)
-    column += 1
-
-
-# the data can be accessed by value[column][row]
-# print a couple of values
-
-if len(values) > 4 and len(values[0]) > 14:
-
-    print()
-
-    column = 3
-    row    = 5
-    print('Column', column, ', name =', names[column], ', type =', types[column], ', units =', units[column], ', value at row', row, '=', values[column][row])
-
-    column = 4
-    row    = 14
-    print('Column', column, ', name =', names[column], ', type =', types[column], ', units =', units[column], ', value at row', row, '=', values[column][row])
-
-
-# look for a specific column name
-
-columnName = 'Radius_1'
-try:
-    whichColumn = names.index(columnName)
-except ValueError:
-    print('\nColumn name', columnName, 'not found!')
-    whichColumn = -1
-
-if whichColumn >= 0:
-    print('\nColumn name', columnName, 'is column index', whichColumn, '(0-based)')
-
-# print a couple of values from the column just found
-
-if len(values[whichColumn]) > 17:
-
-    print()
-
-    column = whichColumn
-
-    row = 3
-    print('Column', column, ', name =', names[column], ', type =', types[column], ', units =', units[column], ', value at row', row, '=', values[column][row])
-
-    row = 16
-    print('Column', column, ', name =', names[column], ', type =', types[column], ', units =', units[column], ', value at row', row, '=', values[column][row])
-
-
-# look for a specific column name that won't (well, shouldn't) be found
-
-columnName = 'This_Should_Not_Be_Found'
-try:
-    whichColumn = names.index(columnName)
-except ValueError:
-    print('\nColumn name', columnName, 'not found!')
+if __name__ == '__main__':
+    main()
